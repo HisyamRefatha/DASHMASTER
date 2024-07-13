5a705953a3896f424696c0e508ff45b5cc4e21f6
HisyamRefatha <refatha76@gmail.com>
2024-07-13 17:54:20 +0700

    setup WEB

---
+++ Presentation/DASHMASTER.WEB/Helper/HelperClient.cs
@@ -0,0 +1,52 @@
+﻿using DASHMASTER.WEB.Models;
+using System.Security.Claims;
+
+namespace DASHMASTER.WEB.Helper
+{
+    public class HelperClient
+    {
+        public const string AUTHENTICATION_SCHEMA = "DASHMASTER";
+        public const string PAGE_COOKIE = "DASHMASTER.Page";
+
+        private readonly ILogger _logger;
+        private string API_URL;
+        public HelperClient(IConfiguration configuration,
+            ILogger<HelperClient> logger
+            )
+        {
+            API_URL = configuration.GetValue<string>("APIBaseUrl");
+            _logger = logger;
+        }
+
+
+        public static (bool Success, TokenModel Result) DecodeToken(HttpContext context)
+        {
+            try
+            {
+                var claims = context.User.Identities?.First().Claims.ToList();
+                if (claims != null && claims.Count() > 0)
+                {
+                    var token = new TokenModel()
+                    {
+                        RawToken = claims?.FirstOrDefault(x => x.Type.Equals("token"))?.Value,
+                        User = new TokenUserModel()
+                        {
+                            Id = Guid.Parse(claims?.FirstOrDefault(x => x.Type.Equals("user_id"))?.Value),
+                            Username = claims?.FirstOrDefault(x => x.Type.Equals("username"))?.Value,
+                            Email = claims?.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Email))?.Value,
+                            Role = claims?.FirstOrDefault(x => x.Type.Equals(ClaimTypes.Role))?.Value
+                        },
+                        //BaseApiUrl = API_URL
+                    };
+                    return (true, token);
+                }
+                else
+                    return (false, null);
+            }
+            catch (Exception)
+            {
+                return (false, null);
+            }
+        }
+    }
+}
---
+++ Presentation/DASHMASTER.WEB/Models/TokenModel.cs
@@ -0,0 +1,19 @@
+﻿namespace DASHMASTER.WEB.Models
+{
+    public class TokenModel
+    {
+        public TokenUserModel User { get; set; }
+        public DateTime ExpiredAt { get; set; }
+        public string RawToken { get; set; }
+        public string RefreshToken { get; set; }
+        public string BaseApiUrl { get; set; }
+    }
+
+    public class TokenUserModel
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
