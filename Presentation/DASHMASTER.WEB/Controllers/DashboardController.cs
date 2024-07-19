eaa1d7e00403b0d0d02aa7cae70236cd06f5fe01
HisyamRefatha <refatha76@gmail.com>
2024-07-19 15:26:13 +0700

    dashboard + assets

---
+++ Presentation/DASHMASTER.WEB/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DASHMASTER.WEB.Controllers
+{
+    public class DashboardController : BaseController<DashboardController>
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
