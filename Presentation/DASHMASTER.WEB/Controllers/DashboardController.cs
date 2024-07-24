2853c3fe5ab1fa144d94bbb379cc7a5641fa39c9
HisyamRefatha <refatha76@gmail.com>
2024-07-24 13:55:02 +0700

    init role validation menu

--- Presentation/DASHMASTER.WEB/Controllers/DashboardController.cs
+++ Presentation/DASHMASTER.WEB/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ namespace DASHMASTER.WEB.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["role"] = HttpContext.User.Claims.Where(d => d.Type == "role").FirstOrDefault().Value;
             return View();
         }
     }
